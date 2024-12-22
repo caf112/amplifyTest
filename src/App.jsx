b52b6f649ebee787ac6e3abc837d2931ccc4d168
caf112 <caf112.gt@gmail.com>
2024-12-23 02:06:31 +0900

    ユーザープール内のユーザーでもAPIを使用できない

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,63 @@
-import React, { useState } from "react"; // 状態管理のためのReactフックをインポート
+import React, { useEffect, useState } from "react"; // 状態管理のためのReactフックをインポート
 
-import { Amplify } from "aws-amplify";
+import { Amplify, API, Auth, graphqlOperation } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react"
 import awsconfig from "./aws-exports";
 import "@aws-amplify/ui-react/styles.css"
+import { listUsers } from "./graphql/queries";
 
 Amplify.configure(awsconfig);
 
 
-// Appコンポーネントの定義
 const App = () => {
+
+  const [users,setUsers] = useState([])
+
+  useEffect(() => {
+    // const checkAuth = async () => {
+    //   try {
+    //     const session = await Auth.currentSession();
+    //     console.log(`Current session token:`, session.getIdToken().getJwtToken());
+    //   }catch (error) {
+    //     console.error(`Authentication error:` ,error);
+    //   }
+    // };
+    // checkAuth();
+    fetchUsers()
+  },[])//無限ループが発生してしまうため、2番目のパラメータとして空の配列を入れる
+
+  const fetchUsers = async () => {
+    try {
+      const userData = await API.graphql(graphqlOperation(listUsers));
+      console.log(`API Response:`,userData);
+      const userList = userData.data.listUsers.items;
+      console.log(`user list`,userList);
+
+      setUsers(userList)
+
+    } catch(error) {
+      console.error(`error on fetching users`,error);
+      
+      if (error.errors) {
+        error.errors.forEach((err) => {
+          console.error("GraphQL error:",err.message,err.path,err.extensions);
+        })
+      }
+    }
+  }
+
   return (
-    <>
+    <div className="App">
     <Authenticator >
       {({signOut,user}) => (
         <div>
-          {console.log(user)}
+          {/* {console.log(user)} */}
           <h1>Welcome,{user?.username}</h1>
           <button onClick={signOut}>signout</button>
         </div>
       )}
     </Authenticator>
-    </>
+    </div>
   );
 };
 
